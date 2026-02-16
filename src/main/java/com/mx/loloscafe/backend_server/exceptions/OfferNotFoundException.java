173ab19b4d51d0ad8e38518a50381a95ec81173e
Misavalmad <misavalmad@gmail.com>
2026-02-16 11:11:41 -0600

    style: changed message in OfferNotFoundException.[DEV-MV]

--- src/main/java/com/mx/loloscafe/backend_server/exceptions/OfferNotFoundException.java
+++ src/main/java/com/mx/loloscafe/backend_server/exceptions/OfferNotFoundException.java
@@ -3,6 +3,6 @@ package com.mx.loloscafe.backend_server.exceptions;
 public class OfferNotFoundException extends RuntimeException {
 
     public OfferNotFoundException(Integer id) {
-        super("Offer not Found with Id: " + id);
+        super("Oferta no encontrada con ID: " + id);
     }
 }
