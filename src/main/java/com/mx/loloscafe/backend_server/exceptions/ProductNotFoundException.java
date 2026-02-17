9a36414d5545a035fc3c283f6ab5b18c860351fe
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-02-17 04:26:12 -0600

    feat: Exception for Product

--- src/main/java/com/mx/loloscafe/backend_server/exceptions/ProductNotFoundException.java
+++ src/main/java/com/mx/loloscafe/backend_server/exceptions/ProductNotFoundException.java
@@ -1,7 +1,7 @@
 package com.mx.loloscafe.backend_server.exceptions;
 
 public class ProductNotFoundException extends RuntimeException {
-  public ProductNotFoundException(String message) {
-    super(message);
-  }
+    public ProductNotFoundException(Integer id) {
+        super("Product not Found with Id: " + id);
+    }
 }
