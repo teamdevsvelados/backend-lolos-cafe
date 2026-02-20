04b6e2a961e66d525cae9c8b79b72c91d2cf885e
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-02-19 18:31:14 -0600

    feat: addition on repository tiny method for find product by id and size

--- src/main/java/com/mx/loloscafe/backend_server/repository/ProductSizePriceRepository.java
+++ src/main/java/com/mx/loloscafe/backend_server/repository/ProductSizePriceRepository.java
@@ -1,4 +1,7 @@
 package com.mx.loloscafe.backend_server.repository;
 
+import java.util.Optional;
+
 public interface ProductSizePriceRepository {
+    Optional<Object> findByProduct_IdAndSize_Id(Integer productId, Integer sizeId);
 }
