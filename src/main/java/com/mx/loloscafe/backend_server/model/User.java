637bceb2930c28383e8102bb9b51492abd9ecd80
Daniel Martínez <danielmtz@192.168.1.7>
2026-02-28 20:51:10 -0600

    [DT-225] Fix: Update findByEmail to return null instead of throwing exception to support POST method [DEV-DM]

--- src/main/java/com/mx/loloscafe/backend_server/model/User.java
+++ src/main/java/com/mx/loloscafe/backend_server/model/User.java
@@ -1,5 +1,6 @@
 package com.mx.loloscafe.backend_server.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import org.hibernate.annotations.Cascade;
 
