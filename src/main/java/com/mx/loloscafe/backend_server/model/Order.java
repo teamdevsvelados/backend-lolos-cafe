c9a3ddfd77b63dc3351b042209473a8182eab861
Fancy Encinas <77sthy.fa@gmail.com>
2026-03-10 21:34:30 -0600

    [DT-181]fix: order entity[DEV-FE]

--- src/main/java/com/mx/loloscafe/backend_server/model/Order.java
+++ src/main/java/com/mx/loloscafe/backend_server/model/Order.java
@@ -2,10 +2,10 @@ package com.mx.loloscafe.backend_server.model;
 
 import com.mx.loloscafe.backend_server.model.enums.OrderStatus;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,13 +16,13 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer idOrder;
+    private Long idOrder;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status_of", nullable = false)
     private OrderStatus statusOf = OrderStatus.CREADO;
 
-    @Column(name = "general_notes")
+    @Column(name = "general_notes", length = 500)
     private String generalNotes;
 
     @Column(nullable = false, precision = 10, scale = 2)
@@ -34,7 +34,8 @@ public class Order {
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal total = new BigDecimal("0.00");
 
-    @Column(name = "date_creation", nullable = false, columnDefinition = "DATETIME")
+    @CreationTimestamp
+    @Column(name = "date_creation", nullable = false, updatable = false)
     private LocalDateTime dateCreation;
 
     //--User (N:1)
@@ -42,95 +43,31 @@ public class Order {
     @JoinColumn(name = "id_user")
     private User user;
 
-    //--OrderItem (1:N)
-    //@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    //private List<OrderItems> orderItems = new ArrayList<>();
-
-    //--Offer (N:1)
-    //@ManyToOne
-    //@JoinColumn(name = "id_offer")
-    //private Offer offer;
-
-
-    public Order(Integer idOrder, User user, LocalDateTime dateCreation, BigDecimal total, BigDecimal discount, BigDecimal subtotal, String generalNotes, OrderStatus statusOf) {
+    public Order(Long idOrder, OrderStatus statusOf, String generalNotes, BigDecimal subtotal, BigDecimal discount, BigDecimal total, LocalDateTime dateCreation, User user) {
         this.idOrder = idOrder;
-        this.user = user;
-        this.dateCreation = dateCreation;
-        this.total = total;
-        this.discount = discount;
-        this.subtotal = subtotal;
-        this.generalNotes = generalNotes;
         this.statusOf = statusOf;
+        this.generalNotes = generalNotes;
+        this.subtotal = subtotal;
+        this.discount = discount;
+        this.total = total;
+        this.dateCreation = dateCreation;
+        this.user = user;
     }
 
-    public Order() {
-    }
-
-    public Integer getIdOrder() {
+    public Long getIdOrder() {
         return idOrder;
     }
 
-    public void setIdOrder(Integer idOrder) {
+    public void setIdOrder(Long idOrder) {
         this.idOrder = idOrder;
     }
-    /*
-    public Offer getOffer() {
-        return offer;
+
+    public OrderStatus getStatusOf() {
+        return statusOf;
     }
 
-    public void setOffer(Offer offer) {
-        this.offer = offer;
-    }
-
-
-    public List<OrderItems> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItems> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-     */
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getDateCreation() {
-        return dateCreation;
-    }
-
-    public void setDateCreation(LocalDateTime dateCreation) {
-        this.dateCreation = dateCreation;
-    }
-
-    public BigDecimal getTotal() {
-        return total;
-    }
-
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-
-    public BigDecimal getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(BigDecimal discount) {
-        this.discount = discount;
-    }
-
-    public BigDecimal getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(BigDecimal subtotal) {
-        this.subtotal = subtotal;
+    public void setStatusOf(OrderStatus statusOf) {
+        this.statusOf = statusOf;
     }
 
     public String getGeneralNotes() {
@@ -141,12 +78,44 @@ public class Order {
         this.generalNotes = generalNotes;
     }
 
-    public OrderStatus getStatusOf() {
-        return statusOf;
+    public BigDecimal getSubtotal() {
+        return subtotal;
     }
 
-    public void setStatusOf(OrderStatus statusOf) {
-        this.statusOf = statusOf;
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
--- src/main/java/com/mx/loloscafe/backend_server/repository/OrderItemsRepository.java
+++ src/main/java/com/mx/loloscafe/backend_server/repository/OrderItemsRepository.java
@@ -8,4 +8,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderItemsRepository extends JpaRepository <OrderItems, Integer> {
 }
-
